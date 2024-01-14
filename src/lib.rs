511422e7fc2603f16f68227c40d6da05829f1abc
CatRass <catrassyt@gmail.com>
2024-01-14 21:31:36 +1100

    Added functions getDriveSize() - Implemented getDriveSize() for Windows version - Began implementing getDrizeSize() for Linux, but having issues with piped output

--- src/lib.rs
+++ src/lib.rs
@@ -18,11 +18,12 @@ mod platform {
     use std::fs;
     use std::os::windows::fs::MetadataExt;
     use chrono::{Utc, DateTime, Local}; 
+    use std::process::Command;
 
     pub fn printDir(currDir: fs::ReadDir){
 
         println!("\x1b[34m{:50} {:15} {:15} {:30}\x1b[0m", "File", "Size (Bytes)", "Read-Only", "Last Modified");
-    
+
         'infoDump: for file in currDir {
     
             let mut fileSpacing = 50;
@@ -41,7 +42,10 @@ mod platform {
     
             let fileName = if fileMetadata.is_dir() {
                 // For coloured file names
-                format!("\x1b[95m{}\x1b[0m",&file.file_name().into_string().unwrap())
+                format!("\x1b[95m{}\x1b[0m",&file.file_name()
+                                            .into_string()
+                                            .unwrap()
+                )
             } else {
                 file.file_name().into_string().unwrap()
             };
@@ -67,6 +71,22 @@ mod platform {
         }
     }
     
+    /// Returns the Size of the C: drive in bytes
+    fn getDriveSize() -> u64{
+        let diskInfo = Command::new("wmic")
+                                .arg("diskdrive")
+                                .arg("get")
+                                .arg("size")
+                                .output()
+                                .expect("Error");
+        let result = String::from_utf8(diskInfo.stdout).expect("Error");
+        let mut driveSize = result.lines();
+
+        let driveSizeB = driveSize.nth(1).expect("uhoh").trim().parse::<u64>().unwrap();
+
+        return driveSizeB;
+    }
+    
 }
 
 #[allow(non_snake_case)]
@@ -74,13 +94,13 @@ mod platform {
 #[allow(unused_imports)]
 mod platform {
     
-    use chrono::{Utc, DateTime, Local};
+    use chrono::{Utc, DateTime, Local, OutOfRange};
     use std::fs;
     use std::os::linux;
+    use std::process::{Command,Stdio};
 
     pub fn printDir(currDir: fs::ReadDir) {
         println!("\x1b[34m{:50} {:15} {:15} {:30}\x1b[0m", "File", "Size (Bytes)", "Read-Only", "Last Modified");
-    
         'infoDump: for file in currDir {
     
             let mut fileSpacing = 50;
@@ -122,6 +142,34 @@ mod platform {
             println!("{:fileSpacing$} {:15} {:15} {:30}", fileName, fileSize, fileMetadata.permissions().readonly(), lastModified);
         }
     }   
+    
+    fn getDriveSize(){
+        let driveSizeCmd =  Command::new("df")
+                            .arg("-T")
+                            .arg("/dev/sda")
+                            .stdout(Stdio::piped())
+                            .spawn()
+                            .expect("Error!");
+
+        let dfResult = driveSizeCmd.stdout.expect("Error");
+        
+        // TODO: Fix the error caused by the last arg.
+        // The error is:
+        // awk: cmd. line:1: '{print $1}'
+        // awk: cmd. line:1: ^ invalid char ''' in expression
+        // Relevant docs: https://doc.rust-lang.org/std/process/index.html#handling-io
+        let pipeResult = Command::new("awk")
+                        .arg("-F ' '")
+                        .arg("'{print $1}'")
+                        .stdin(Stdio::from(dfResult))
+                        .stdout(Stdio::piped())
+                        .spawn()
+                        .expect("Error");
+        
+        let varStr = pipeResult.wait_with_output().unwrap(); 
+        println!("{}",String::from_utf8(varStr.stdout).unwrap());
+    }
+
 }
 
 #[allow(non_snake_case)]
